821a821c1cc75e7e7274eef0d8a4fd2f6d5bb9f1
eric98 <ergare.17@gmail.com>
2018-05-15 19:46:45 +0200

    Sync

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,4 @@
+
+:
+
+js/game.js,a/1/a11dffb636ca569515545ae88e1b79af1707c694
--- js/game.js
+++ js/game.js
@@ -1,82 +1,85 @@
 
-var game = new Phaser.Game(800, 240, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
-
-function preload() {
-
-  game.load.tilemap('mario', 'assets/tilemaps/maps/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.image('tiles', 'assets/tilemaps/tiles/super_mario.png');
-  game.load.spritesheet('player','assets/player16x16.png', 14, 11)
-
-}
-
 var map;
 var layer;
 var player;
 var cursors;
+var level1 = {
+  preload() {
 
-function create() {
+    game.load.tilemap('mario', 'assets/tilemaps/maps/super_mario.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/tilemaps/tiles/super_mario.png');
+    game.load.spritesheet('player','assets/player16x16.png', 14, 11)
 
-  game.stage.backgroundColor = '#787878';
+  },
+  create() {
 
-  map = game.add.tilemap('mario');
+    game.stage.backgroundColor = '#787878';
 
-  map.addTilesetImage('SuperMarioBros-World1-1', 'tiles');
+    map = game.add.tilemap('mario');
 
-  layer = map.createLayer('World1');
+    map.addTilesetImage('SuperMarioBros-World1-1', 'tiles');
 
-  layer.resizeWorld();
+    layer = map.createLayer('World1');
 
-  layer.wrap = true;
+    layer.resizeWorld();
 
-  cursors = game.input.keyboard.createCursorKeys();
+    layer.wrap = true;
 
-  map.setCollisionBetween(15, 16);
-  map.setCollisionBetween(20, 25);
-  map.setCollisionBetween(27, 29);
-  map.setCollision(40);
+    cursors = game.input.keyboard.createCursorKeys();
 
-  player = game.add.sprite(250,50,'player')
+    map.setCollisionBetween(15, 16);
+    map.setCollisionBetween(20, 25);
+    map.setCollisionBetween(27, 29);
+    map.setCollision(40);
 
-  game.physics.arcade.enable(player)
+    player = game.add.sprite(250,50,'player')
 
-  player.animations.add('idle',[3,4,5,4],5,true)
+    game.physics.arcade.enable(player)
 
-  game.physics.enable(player);
+    player.animations.add('idle',[3,4,5,4],5,true)
 
-  game.physics.arcade.gravity.y = 250;
+    game.physics.enable(player);
 
-  player.body.linearDamping = 1;
-  player.body.collideWorldBounds = true;
+    game.physics.arcade.gravity.y = 250;
 
-  game.camera.follow(player);
+    player.body.linearDamping = 1;
+    player.body.collideWorldBounds = true;
 
-}
+    game.camera.follow(player);
 
-function update() {
+  },
+  update() {
 
-  player.animations.play('idle')
+    player.animations.play('idle')
 
-  game.physics.arcade.collide(player, layer);
+    game.physics.arcade.collide(player, layer);
 
-  player.body.velocity.x = 0;
+    player.body.velocity.x = 0;
 
-  if (cursors.up.isDown)
-  {
-    if (player.body.onFloor())
+    if (cursors.up.isDown)
     {
-      player.body.velocity.y = -200;
+      if (player.body.onFloor())
+      {
+        player.body.velocity.y = -200;
+      }
+    }
+
+    if (cursors.left.isDown)
+    {
+      player.body.velocity.x = -150;
+      player.frame = 2
+    }
+    else if (cursors.right.isDown)
+    {
+      player.body.velocity.x = 150;
+      player.frame = 1
     }
   }
-
-  if (cursors.left.isDown)
-  {
-    player.body.velocity.x = -150;
-    player.frame = 2
-  }
-  else if (cursors.right.isDown)
-  {
-    player.body.velocity.x = 150;
-    player.frame = 1
-  }
-
 }
+
+var game = new Phaser.Game(800, 240, Phaser.AUTO, 'game', level1);
+
+game.state.add('menu',menu)
+game.state.add('level1',level1)
+game.state.add('level2',level2)
+game.state.start('level1')
